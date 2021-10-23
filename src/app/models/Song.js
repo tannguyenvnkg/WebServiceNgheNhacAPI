b2aa71cf55c4d7f14b2336860394d6e763cb9e1f
Tan Nguyen <tannguyenvnkg@gmail.com>
2021-10-23 18:42:42 +0700

    hoàn thành tìm kiếm theo chủ đề, tên bài hát

--- src/app/models/Song.js
+++ src/app/models/Song.js
@@ -2,11 +2,19 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 // const ObjectId = Schema.ObjectId;
 
+const Category = new Schema({
+   _id: String,
+   categoryname: String 
+});
+
+
 const Song = new Schema({
     _id: Schema.Types.ObjectId,
     image: String,
     link: String,
-    title: String 
+    title: String,
+    category: {type: [Category], index: true},
+    description: String
 });
 
 module.exports = mongoose.model('Song', Song);
