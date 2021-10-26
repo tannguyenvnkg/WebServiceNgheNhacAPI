6f1f5261c57453a91457d232d4f7a7fa5ade9bab
Tan Nguyen <tannguyenvnkg@gmail.com>
2021-10-26 16:04:28 +0700

    -  hoàn thành trang insert playlist  - hoàn thành trang show hình ảnh playlist =>  http://localhost:3000/image/imageplaylist/ (tên hình).jpg hoặc .png * Insert vào mongoDB sẽ lưu đường dẫn của trang web show hình ảnh * toàn bộ input insert vào trang web sẽ được required => bắt buộc phải điền đầy đủ thông tin kể cả file upload

--- src/routes/Image.route.js
+++ src/routes/Image.route.js
@@ -2,8 +2,9 @@ const express = require('express');
 const router = express.Router();
 
 const imageSingerController = require('../app/controllers/Image/ImageSingerController');
+const imagePlaylistController = require('../app/controllers/Image/ImagePlaylistController');
 
-
+router.use('/imageplaylist/:imageName',imagePlaylistController.display);
 router.use('/imagesinger/:imageName',imageSingerController.display);
 router.use('/',imageSingerController.index);
 
--- src/routes/Insert.route.js
+++ src/routes/Insert.route.js
@@ -1,10 +1,12 @@
 const express = require('express');
-const multer = require('multer');
-const path = require('path');
 const router = express.Router();
 
 const InsertCategoryController = require('../app/controllers/Insert/InsertCategoryController');
 const InsertSingerController = require('../app/controllers/Insert/InsertSingerController');
+const InsertPlaylistController = require('../app/controllers/Insert/InsertPlaylistController');
+
+const {uploadImageSinger} = require('./Upload.multer');
+const {uploadImagePlaylist} = require('./Upload.multer');
 // const InsertControllerAutomaly = require('../app/controllers/InsertControllerAutomaly');
 
 
@@ -17,23 +19,14 @@ const InsertSingerController = require('../app/controllers/Insert/InsertSingerCo
 // // ==========================================================================
 
 
-//insert song 
+//insert category 
 router.get('/insertCategory',InsertCategoryController.insertCategory);
 router.post('/insertCategoryPost',InsertCategoryController.insertCategoryPost);
+//insert playlist 
+router.get('/insertPlaylist',InsertPlaylistController.insertPlaylist);
+router.post('/insertPlaylistPost', uploadImagePlaylist.single('imagePlaylist'),InsertPlaylistController.insertPlaylistPost);
 
 //insert singer
-var storageImageSinger = multer.diskStorage({
-    destination: function (req, file, cb) {
-    const pathSingerImage = path.join(__dirname, '../','public','image','singer','/');
-      cb(null, pathSingerImage)
-    },
-    filename: function (req, file, cb) {
-      cb(null,  Date.now() + file.originalname )
-    }
-  })
-   
-  var uploadImageSinger = multer({ storage: storageImageSinger })
-
 router.get('/insertSinger',InsertSingerController.insertSinger);
 router.post('/insertSingerPost', uploadImageSinger.single('image'),InsertSingerController.insertSingerPost);
 //==========================================================================
