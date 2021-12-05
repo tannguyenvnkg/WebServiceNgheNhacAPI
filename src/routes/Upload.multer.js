da6d407df0f627780254e5757627cf83c31cca83
Tan Nguyen <tannguyenvnkg@gmail.com>
2021-12-05 11:52:22 +0700

    - clean code

--- src/routes/Upload.multer.js
+++ src/routes/Upload.multer.js
@@ -4,26 +4,26 @@ const path = require('path');
 // upload image singer
 var storageImageSinger = multer.diskStorage({
     destination: function (req, file, cb) {
-    const pathSingerImage = path.join(__dirname, '../','public','image','singer','/');
+      const pathSingerImage = path.join(__dirname, '../','public','image','singer','/');
       cb(null, pathSingerImage)
     },
     filename: function (req, file, cb) {
       cb(null,  Date.now() + file.originalname )
     }
-  })
+});
    
 var uploadImageSinger = multer({ storage: storageImageSinger })
 //===============================================================================
 //upload image playlist
 var storageImagePlaylist = multer.diskStorage({
   destination: function (req, file, cb) {
-  const pathPlaylistImage = path.join(__dirname, '../','public','image','playlist','/');
+    const pathPlaylistImage = path.join(__dirname, '../','public','image','playlist','/');
     cb(null, pathPlaylistImage)
   },
   filename: function (req, file, cb) {
     cb(null,  Date.now() + file.originalname )
   }
-})
+});
  
 var uploadImagePlaylist = multer({ storage: storageImagePlaylist })
 //=====================================================================
@@ -43,33 +43,33 @@ var storageImageSong = multer.diskStorage({
   filename: function (req, file, cb) {
     cb(null,  Date.now() + file.originalname )
   }
-})
+});
  
 var uploadImageSong = multer({ storage: storageImageSong })
 //=====================================================================
 //upload image Album
 var storageImageAlbum = multer.diskStorage({
   destination: function (req, file, cb) {
-  const pathAlbumImage = path.join(__dirname, '../','public','image','album','/');
+    const pathAlbumImage = path.join(__dirname, '../','public','image','album','/');
     cb(null, pathAlbumImage)
   },
   filename: function (req, file, cb) {
     cb(null,  Date.now() + file.originalname )
   }
-})
+});
  
 var uploadImageAlbum = multer({ storage: storageImageAlbum })
 //=====================================================================
 //upload image Category
 var storageImageCategory = multer.diskStorage({
   destination: function (req, file, cb) {
-  const pathCategoryImage = path.join(__dirname, '../','public','image','category','/');
+    const pathCategoryImage = path.join(__dirname, '../','public','image','category','/');
     cb(null, pathCategoryImage)
   },
   filename: function (req, file, cb) {
     cb(null,  Date.now() + file.originalname )
   }
-})
+});
  
 var uploadImageCategory = multer({ storage: storageImageCategory })
 //=====================================================================
